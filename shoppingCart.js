fbd707b87e197dfc14723bcb8e31a2fd5ebb1b11
Yuhao-nyc <jimfengyh@gmail.com>
2017-03-29 17:45:05 -0400

    added new functionality
    scan products and calculate the total cost

--- shoppingCart.js
+++ shoppingCart.js
@@ -20,25 +20,51 @@ if ((volumeA || volumeB || volumeC || volumeD) < 0) {
     volumeD = 0;
 }
 
-var dividA = volumeA % 4,
-    dividC = volumeC % 6;
-
-if (volumeA >= 4) {
-    var priceAdis = 7/4;
-    resultA = priceAdis*(volumeA - dividA) + priceA*dividA;
-}
-
-if (volumeC >= 6) {
-    var priceCdis = 6/6;
-    resultC = priceCdis*(volumeC - dividC) + priceC*dividC;
-}
-
 //quantity of products input
+//TODO count the item quantity with a countVolume function
 volumeA = 4;
 volumeB = 2;
 volumeC = 1;
 volumeD = 1;
 
-total = resultA + resultB + resultC + resultD;
+var dividA = volumeA % 4,
+    dividC = volumeC % 6;
 
-console.log(total.toFixed(2))
+function scan(product) {
+    if (product == 'A') {
+       if (volumeA >= 4) {
+            var priceAdis = 7/4;
+            resultA = priceAdis*(volumeA - dividA) + priceA*dividA;
+       }
+       return resultA
+    }
+
+    if (product == 'B') {
+        return resultB
+    }
+
+    if (product == 'C') {
+       if (volumeC >= 6) {
+          var priceCdis = 6/6;
+          resultC = priceCdis*(volumeC - dividC) + priceC*dividC;
+       }
+       return resultC;
+    }
+
+    if (product == 'D') {
+       return resultD;
+    }
+}
+
+function total() {
+    return scan('A') + scan('B') + scan('C') + scan('D');
+}
+
+//TODO set prices
+function setPricing(item, itemPrice) {
+
+}
+
+setPricing('A', 10);
+
+console.log(total())
